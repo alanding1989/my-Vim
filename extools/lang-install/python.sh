95fa244de60ce0321d6ef1dc40c654980ab5388f
alanding <395577197@qq.com>
2019-06-06 16:14:36 +0800

    update

--- extools/lang-install/python.sh
+++ extools/lang-install/python.sh
@@ -7,6 +7,15 @@
 # Description  : 
 #================================================================================
 
+dotfile="$ALANDOTFILE"
+
+if ! echo "$PATH" | grep conda ; then
+  cpath=$(pwd)
+  cd /mnt/fun+downloads/linux系统安装/code-software/lang/python || return
+  curl -LO https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh
+  sh ./Miniconda3-latest-Linux-x86_64.sh
+  cd "$cpath" || return
+fi
 
 if [ ! -x "/usr/bin/pip" ]; then
   ln -s /home/alanding/software/anaconda3/envs/py36/bin/pip usr/bin/pip
@@ -16,3 +25,9 @@ if ! grep -q PYSPARK_PYTHON "/etc/profile"; then
   print "export JAVA_HOME=/opt/lang-tools/java/jdk \
     \nexport PYSPARK_PYTHON=/home/alanding/software/anaconda3/envs/py36/bin/python3.6" >> /etc/profile
 fi
+
+if [ ! -e "/home/alanding/software/anaconda3/envs/py36" ]; then
+  conda creat -n py36 python=3.6
+  pip install -r "$dotfile/pip3-list.txt"
+fi
+
--- extools/plug-install/cpp-ccls.sh
+++ extools/plug-install/cpp-ccls.sh
@@ -9,11 +9,15 @@
 
 cclspath=/mnt/fun+downloads/linux系统安装/code-software/lang/cpp/ccls
 
-if [ ! -e $cclspath ]; then
-  cd $(dirname $cclspath) && git clone --depth=1 --recursive https://github.com/MaskRay/ccls && cd ccls
-else
-  cd $cclspath && git pull
-fi
+[ -e $cclspath ] && rm -rf "$cclspath"
 
-cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release -DCMAKE_PREFIX_PATH=/opt/lang-tools/cpp/clang -DCMAKE_INSTALL_PREFIX=/opt/lang-tools/cpp/clang
+cd "$(dirname $cclspath)" && \
+  git clone --depth=1 --recursive https://github.com/MaskRay/ccls && \
+  cd ccls || return
+
+cmake -H. -BRelease -DCMAKE_BUILD_TYPE=Release \
+  -DCMAKE_PREFIX_PATH=/opt/lang-tools/cpp/clang \
+  -DCMAKE_INSTALL_PREFIX=/opt/lang-tools/cpp/clang
+
 cmake --build Release --target install
+
