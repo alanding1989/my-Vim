81e6cd0a562b2a782b83b90738cdd431d8fc6fe2
alanding <395577197@qq.com>
2019-07-25 13:35:29 +0800

    update nodejs

--- extools/lang-install/nodejs.sh
+++ extools/lang-install/nodejs.sh
@@ -9,10 +9,11 @@
 
 
 if [ ! -e "/opt/lang-tools/nvm" ]; then
-  git clone https://github.com/creationix/nvm.git /opt/lang-tools/nvm
-  nvm install v11.9.0
-  nvm alias default v11.9.0
-  source "$HOME/.zshrc"
+  git clone git@github.com:nvm-sh/nvm.git /opt/lang-tools/nvm
+  nvm install --lts && nvm use --lts
+  nvm alias default "$(nvm version)"
+  \. "$HOME/.zshrc"
+  # nvm install node --reinstall-packages-from=node
 fi
 
 nodepath=$(which node)
@@ -20,23 +21,14 @@ ln -s "$nodepath" /usr/bin/node
 ln -s "$nodepath" /usr/bin/nodejs
 
 npm -g i nrm && nrm use taobao
-npm config set "@fortawesome:registry" https://npm.fontawesome.com/ && \
-  npm config set "//npm.fontawesome.com/:_authToken" TOKEN
-
 npm -g i yarn
 yarn config set registry 'https://registry.npm.taobao.org'
-npm -g i rimraf
-npm -g i neovim
-npm -g i import-js
-npm -g i remark
-npm -g i remark-cli
-npm -g i remark-stringify
-npm -g i remark-frontmatter
-npm -g i wcwidth
-npm -g i bash-language-server
-npm -g i javascript-typescript-langserver
-npm -g i vscode-css-languageserver-bin
-npm -g i --save-dev @fortawesome/fontawesome-free
-yarn global add prettier
-yarn global add diagnostic-languageserver
-yarn global add vim-language-server
+
+npm -g i --unsafe-perm bash-language-server
+npm -g i neovim import-js remark remark-cli remark-stringify remark-frontmatter \
+rimraf wcwidth prettier diagnostic-languageserver vim-language-server \
+javascript-typescript-langserver vscode-css-languageserver-bin \
+
+# npm -g i --save-dev @fortawesome/fontawesome-free
+# npm config set "@fortawesome:registry" https://npm.fontawesome.com/ && \
+  # npm config set "//npm.fontawesome.com/:_authToken" TOKEN
